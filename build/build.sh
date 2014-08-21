88678a15b49f15a8c31f85a33d32228cf4be4c52
m8rge <to.merge@gmail.com>
2014-08-21 11:25:57 +0600

    fix build script

--- build/build.sh
+++ build/build.sh
@@ -5,7 +5,7 @@ php /usr/bin/phar.phar pack \
 -f bin/servicecheck.phar \
 -s pharStub.php \
 -c gz \
--i "index\.php|src|vendor" \
+-i ".+\.php|vendor" \
 -x "\.git|tests?|\.DS_Store|README*|AUTHORS|CHANGES|Makefile|phpunit\.xml*|\.travis\.yml|composer\.+" \
 ./..
 chmod +x bin/servicecheck.phar
