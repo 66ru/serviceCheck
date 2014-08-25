a05ad6ee62dfa4e56a2029cf22f6e6cda73b1d39
m8rge <to.merge@gmail.com>
2014-08-25 14:38:44 +0600

    error reporting, autoload fix

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+
 /** @var \Composer\Autoload\ClassLoader $autoLoader */
 $autoLoader = require_once 'vendor/autoload.php';
-$autoLoader->addPsr4('m8rge\\OCF\\', '');
+$autoLoader->addPsr4('m8rge\\OCF\\', __DIR__);
 
 if ($argc == 2 && !empty($argv[1])) {
     $action = $argv[1];
